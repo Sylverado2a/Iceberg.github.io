76d26366c85328fd2895909edc0c6fbd79723055
Sylverado2a <123154494+Sylverado2a@users.noreply.github.com>
2023-04-07 18:55:12 +0200

    Add files via upload

---
+++ wario.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Iceberg Super Mario 64</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <style>
+        body{
+            background: black;
+            width: 98vw;
+            height: 100vh;
+        }
+        .casquette{
+            position: relative;
+        }
+    </style>
+    <video width="100%" height="100%" autoplay>
+      <source src="wario-attack.mp4" type=video/mp4>
+    </video>
+</body>
+</html>
